511b163c95a2de1a1115d528f40af4845904fa60
Bologov Svyatoslav <Bologov885@yandex.ru>
2025-05-24 00:48:41 +0300

    Выполнение домашнего задания №1.

--- run.sh
+++ run.sh
@@ -1,1 +1,21 @@
-# TODO: заполнить
+#!/bin/bash
+
+echo "✅ Скрипт запущен"
+
+IMAGE_NAME=vsu-amm-bot
+CONTAINER_NAME=vsu-amm-bot-container
+
+echo "🛠️ Сборка Docker образа..."
+docker build -t $IMAGE_NAME .
+
+echo "🧹 Остановка и удаление предыдущего контейнера (если есть)..."
+docker stop $CONTAINER_NAME 2>/dev/null || true
+docker rm $CONTAINER_NAME 2>/dev/null || true
+
+echo "🚀 Запуск нового контейнера..."
+docker run -d \
+    --name "$CONTAINER_NAME" \
+    --env-file .env \
+    "$IMAGE_NAME"
+
+echo "✅ Контейнер $CONTAINER_NAME запущен."
