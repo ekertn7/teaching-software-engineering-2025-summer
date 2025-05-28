d564f7661f7305795388c253257377b29d0e8242
Leshhh-afk <lesh3333@gmail.com>
2025-05-28 22:39:01 +0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-# TODO: заполнить
+#!/bin/bash
+
+docker rm -f helper_bot_container 2>/dev/null || true
+
+docker rmi helper_bot 2>/dev/null || true
+
+docker build -t helper_bot .
+docker run -d --rm --name helper_bot_container  helper_bot
