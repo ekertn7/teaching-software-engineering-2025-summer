4b8f978c271c0e4a4bbc9d5a08c4d60e66da374c
RodionovAnna <newuser@LAPTOP-2J3802CE.>
2025-05-29 14:02:51 +0300

    Выполнение домашнего задания №1.

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-# TODO: заполнить
+#!/bin/bash
+IMAGE_NAME="my-bot"
+CONTAINER_NAME="my-bot-container"
+docker build -t $IMAGE_NAME 
+docker rm -f $CONTAINER_NAME 2>/dev/null || true
+docker run -d \
+  --name $CONTAINER_NAME \
+  --env-file .env \
+  --restart unless-stopped \
+  $IMAGE_NAME
+echo "Контейнер $CONTAINER_NAME запущен на основе образа $IMAGE_NAME"
