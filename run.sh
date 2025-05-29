a11f8d918403e3b8863c2a2a785453d157d14766
ty-shyas-uidesh <01001000@inbox.ru>
2025-05-29 15:14:31 +0300

    Фокин Дмитрий. Домашнее задание №1

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-# TODO: заполнить
+docker build -t telegram_bot .
+
+docker rm -f telegram_bot_container 2>/dev/null || true
+
+docker run -d --rm --name telegram_bot_container --env-file .env telegram_bot
+
+echo "Бот успешно запущен в контейнере telegram_bot_container"
