1b0acc34d5b44f54f9b5f1d58cdcf653a79a1681
Артюшина Юлия <youlqw11111@gmail.com>
2025-05-29 14:54:20 +0300

    Выполнение домашнего задания №1.

--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
-# TODO: заполнить
+IMAGE_NAME="my-bot-image"
+CONTAINER_NAME="my-bot-container"
+
+docker build -t $IMAGE_NAME .
+
+docker rm -f $CONTAINER_NAME 2>/dev/null || true
+
+docker run -d \
+  --name $CONTAINER_NAME \
+  --env-file .env \
+  --restart unless-stopped \
+  $IMAGE_NAME
+
+echo "Бот успешно запущен в контейнере $CONTAINER_NAME"
